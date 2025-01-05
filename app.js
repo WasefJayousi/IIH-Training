8c76cfbbf07cda57ac48f36fa98359413a649b26
WasefJayousi <wfarid311@gmail.com>
2025-01-05 15:13:48 +0300

    5 methods of list

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 
 let lst = [1,2,3,4,5,6,7,8,9,10]
-let lst2 = ["wasef" , "farid"]
 
 //اضافة عنصر في اخر المصفوفة , وارجاع اعداد المصفوفة 
 console.log(`The added element: ${lst.push(11)}`)
@@ -14,6 +13,8 @@ console.log("The index of the element 5 : " + lst.indexOf(5))
 
 unsorted_lst = [2,4,3,5,6,7,10,8,9,1]
 //ترنيب المصفوفة من اصغر الى اكبر
-console.log(unsorted_lst.sort())
+console.log("unsorted to sorted: "+unsorted_lst.sort())
 
-console.log(lst.unshift(0))
+//اضافة عناصر جديدة في بداية المصفوفة
+lst.unshift(0)
+console.log("added 0 at the start of the array: "+lst)
